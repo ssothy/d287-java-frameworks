aae3b39144917e453b64128c2c547087a50a8a86
Sami Sothy <ssothy@wgu.edu>
2024-02-14 15:16:42 -0800

    Omg I think I figured out mapping and controllers now.

--- src/main/java/com/example/demo/controllers/MainScreenControllerr.java
+++ src/main/java/com/example/demo/controllers/MainScreenControllerr.java
@@ -10,6 +10,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
@@ -22,8 +23,8 @@ import java.util.List;
 
 @Controller
 public class MainScreenControllerr {
-   // private final PartRepository partRepository;
-   // private final ProductRepository productRepository;'
+    // private final PartRepository partRepository;
+    // private final ProductRepository productRepository;'
 
     private PartService partService;
     private ProductService productService;
@@ -31,25 +32,37 @@ public class MainScreenControllerr {
     private List<Part> theParts;
     private List<Product> theProducts;
 
+
  /*   public MainScreenControllerr(PartRepository partRepository, ProductRepository productRepository) {
         this.partRepository = partRepository;
         this.productRepository = productRepository;
     }*/
 
-    public MainScreenControllerr(PartService partService,ProductService productService){
-        this.partService=partService;
-        this.productService=productService;
+    public MainScreenControllerr(PartService partService, ProductService productService) {
+        this.partService = partService;
+        this.productService = productService;
+
     }
+
     @GetMapping("/mainscreen")
-    public String listPartsandProducts(Model theModel, @Param("partkeyword") String partkeyword, @Param("productkeyword") String productkeyword){
+    public String listPartsandProducts(Model theModel, @Param("partkeyword") String partkeyword, @Param("productkeyword") String productkeyword) {
         //add to the sprig model
-        List<Part> partList=partService.listAll(partkeyword);
-        theModel.addAttribute("parts",partList);
-        theModel.addAttribute("partkeyword",partkeyword);
-    //    theModel.addAttribute("products",productService.findAll());
-        List<Product> productList=productService.listAll(productkeyword);
+        List<Part> partList = partService.listAll(partkeyword);
+        theModel.addAttribute("parts", partList);
+        theModel.addAttribute("partkeyword", partkeyword);
+        //    theModel.addAttribute("products",productService.findAll());
+        List<Product> productList = productService.listAll(productkeyword);
         theModel.addAttribute("products", productList);
-        theModel.addAttribute("productkeyword",productkeyword);
+        theModel.addAttribute("productkeyword", productkeyword);
         return "mainscreen";
     }
+
+    public class about {
+        @RequestMapping("about")
+        public String getAbout() {
+            return "about";
+        }
+    }
+
 }
+
--- src/main/java/com/example/demo/controllers/about.java
+++ src/main/java/com/example/demo/controllers/about.java
@@ -1,15 +1,25 @@
 package com.example.demo.controllers;
 
+import com.example.demo.domain.Part;
+import com.example.demo.domain.Product;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 
-public class AboutController {
-    @RequestMapping("/about")
+public class about {
+    @RequestMapping("about")
     public String getAbout() {
         return "about";
     }
+
 }
+
+
+
+
